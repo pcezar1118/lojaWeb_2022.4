c0009baccef43116e5be44f125ea66d28db4b7f1
pcezar1118 <pcezar1118@gmail.com>
2022-11-19 20:45:44 -0200

    dao

---
+++ api/model/index.php
@@ -0,0 +1,25 @@
+<?php
+include_once("./config/config.php");
+include_once("./model/Usuario.php");
+
+$method = $_SERVER["REQUEST_METHOD"];
+$router = $_SERVER["PATH_INFO"];
+
+var_dump($_SERVER);
+
+if($method == "POST"){
+    if ($router == "/usuario/add"){
+        try {     
+            $dadosEntrada = json_decode(file_get_contents('php://input'));
+            var_dump($dadosEntrada);
+            $user = new Usuario();
+            $user->nome = $dadosEntrada->nome;
+            $user->email = $dadosEntrada->email;
+            $user->senha = $dadosEntrada->senha;
+            $user->add();        
+            echo "Usuario Cadastrado!";
+        } catch (Exception $e) {
+            echo "Erro:" . $e->getMessage();
+        }
+    }
+}
